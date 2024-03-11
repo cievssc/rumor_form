52493f2d4fe95f92aa5b9a308dc8451cc4d3ed4c
dimitribessa <dimitribessa@gmail.com>
2024-03-11 15:06:39 -0300

    ajustado as queries de monitora_valores e envio de confirmação

--- server/server_monitora.R
+++ server/server_monitora.R
@@ -4,10 +4,13 @@
   #lista de rumores a serem monitorados
   observeEvent(input$navbar == 'Monitora' | input$monitora_enviar,{
                     lista_evento <- DBI::dbGetQuery(conn(), "SELECT id, data_noticia, doenca, area_tecnica FROM rumores_evento WHERE
-                                            id IN (SELECT id FROM rumores_verifica WHERE risc_avalia IN ('Alto', 'Muito alto')) 
+                                            id IN (SELECT id FROM rumores_verifica WHERE risc_avalia IN ('Alto', 'Muito alto'))  AND
+                                                  id NOT IN (SELECT id FROM rumores_monitora WHERE monitora_encerra = 'TRUE')
                                             ")
                     lista_verific <- DBI::dbGetQuery(conn(), "SELECT id, risc_avalia FROM rumores_verifica 
-                                            WHERE risc_avalia IN ('Alto', 'Muito alto')")
+                                            WHERE risc_avalia IN ('Alto', 'Muito alto') AND
+                                                  id NOT IN (SELECT id FROM rumores_monitora WHERE monitora_encerra = 'TRUE')
+                                                  ")
                     lista_monitoradas <- dplyr::left_join(lista_evento, lista_verific, by = 'id')
                      monitora_rumores(lista_monitoradas)
   })
@@ -52,7 +55,7 @@
             checkboxGroupInput("monitora_lista_acoes", label = h3("Ações Realizadas"), 
                 choices = list('Nota Informativa', 'Nota técnica', 'Alerta epidemiológico', 'COES', "Sala de situação")),
             selectizeInput('monitora_area_tecnica', 'Área Técnica envolvida', choices = equipes_monitora, selected = NULL, options = list(
-                      onInitialize = I('function() { this.setValue(""); }'))),
+                      onInitialize = I('function() { this.setValue(""); }')), multiple = T),
             checkboxInput('monitora_desfecho', label = 'Rumor já tem desfecho?',  value = F) 
         )
         }
@@ -115,3 +118,62 @@
        if(input$monitora_agravo == 'Outra'){shinyjs::show('monitora_outroagravo')}else{shinyjs::hide('monitora_outroagravo')} 
   }, ignoreNULL = T) #observe para ocultar o input de outro agravo
 
+ #-------------------------------------------------------------------------
+  #enviando dados
+ #-------------------------------------------------------------------------
+
+  #criando o df dos dados inseridos
+  monitora_dados_empilhados <- reactive({
+                                if(!isTRUE(input$monitora_rumor)){
+                                    data.frame('id' = monitora_selected(),
+                                           'send_usuario_monitora' = check_user()[,'usuario'],
+                                           'send_time_monitora'   = Sys.time(),
+                                           'monitora_acoes' = paste(input$monitora_lista_acoes, collapse = '|'),
+                                           'monitora_equipes' = paste(input$monitora_area_tecnica, collapse = '|'),
+                                           'monitora_encerra' = input$monitora_desfecho)
+
+                                }else{
+                                  data.frame('id' = monitora_selected(),
+                                           'send_usuario_monitora' = check_user()[,'usuario'],
+                                           'send_time_monitora'   = Sys.time(),
+                                           'monitora_acoes' = paste(input$monitora_lista_acoes, collapse = '|'),
+                                           'monitora_equipes' = paste(input$monitora_area_tecnica, collapse = '|'),
+                                           'monitora_encerra' = input$monitora_desfecho,
+                                           'monitora_dt_fim' = input$monitora_datafim,
+                                           'monitora_casos' = input$monitora_casos,
+                                           'monitora_obitos' = input$monitora_obitos,
+                                           'monitora_agravo' = if(input$monitora_agravo == 'Outra'){input$monitora_outroagravo}else{input$monitora_agravo})
+                                }
+                                    })                   
+   
+
+ #--------------------------------------------------------------------------
+ #modal confirmação
+ #--------------------------------------------------------------------------
+  
+ #retorno de confirmação
+ # aparecer o formulário inicial de senha
+ observeEvent(input$monitora_enviar, {
+  
+  dadoi <-  monitora_dados_empilhados()
+ #-----------------------------------------------------------------------------
+ #reiniciando todos os inputs
+  shinyjs::reset(id = 'monitora')
+  
+ #-----------------------------------------------------------------------------
+   
+ DBI::dbWriteTable(conn(),value = dadoi, name = "rumores_monitora", append = T)    
+ 
+ on.exit(DBI::dbDisconnect(conn()))
+ 
+  showModal(modalDialog(
+      title = NULL,
+       tagList(
+        p("Notificação enviada com sucesso")),
+        easyClose = TRUE,
+        footer = NULL
+      )
+      )
+  })  #end observe event
+
+  #TODO >> atualização de status de monitoramento!!!
